c913dc2b69459e4c05491f271088c4a586db8f10
Valentino Chatrian <ICTS22-24.062@studenti.local>
2023-03-30 11:38:59 +0200

    aggiunta somma e input
    agginto mettodo somma e input nel main

--- Calcolatrice.py
+++ Calcolatrice.py
@@ -1,6 +1,14 @@
-class Calcolatrice:
-    pass
+class Calcolatrice():
+    def __init__(self,n1,n2) -> None:
+        self.__n1=n1
+        self.__n2=n2
+    def somma(self):
+        return self.__n1+self.__n2
 
 def main():
-    pass
+  
+    n1=float(input("dammi un numero: "))
+    n2=float(input("dammi un  altro numero: "))
+    calc=Calcolatrice(n1,n2)
+    print("somma: ",calc.somma())
 main()
